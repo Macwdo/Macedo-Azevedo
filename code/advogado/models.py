059015ebd7ef23aeea7bfa6e7f0b35a355e8efb5
Danilo Macedo Azevedo Fernandes <101714528+Macwdo@users.noreply.github.com>
2023-04-11 15:00:27 -0300

    fixtures

--- code/advogado/models.py
+++ code/advogado/models.py
@@ -10,7 +10,7 @@ class Advogado(models.Model):
     nome = models.CharField(max_length=40, blank=False)
     email = models.CharField(max_length=50)
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    oab = models.CharField(max_length=8, unique=True, null=False, blank=True)
+    oab = models.CharField(max_length=10, unique=True, null=False, blank=True)
     image = models.ImageField(upload_to="images/", default=None, null=True)
 
     def __str__(self) -> str:
