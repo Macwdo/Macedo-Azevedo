53f5bd74c53f0acc8c74a545783c38e20de5a1be
Macwdo <danilo.macedofernandes@hotmail.com>
2023-05-10 04:21:30 -0300

    django cleanups

--- code/processo/apps.py
+++ code/processo/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class ProcessoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'processo'
-
-
-    def ready(self, *args, **kwargs) -> None:
-        import processo.signals
-        super_ready = super().ready(*args, **kwargs)
-        return super_ready
