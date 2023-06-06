a999bc52c423750c6df46d0a28870003fbc47133
Macwdo <danilo.macedofernandes@hotmail.com>
2023-06-06 15:41:55 -0300

    lawyer name null true

--- code/advogado/models.py
+++ code/advogado/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 
 class Advogado(models.Model):
-    name = models.CharField(max_length=255, blank=False)
+    name = models.CharField(max_length=255, blank=False, null=True)
     email = models.CharField(max_length=255)
     cpf = models.CharField(max_length=14, null=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="user")
