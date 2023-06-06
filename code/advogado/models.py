41d172cb0d62e495e1561c3dd087d4e632524808
Macwdo <danilo.macedofernandes@hotmail.com>
2023-06-06 14:40:03 -0300

    lawyer page starting

--- code/advogado/models.py
+++ code/advogado/models.py
@@ -5,12 +5,21 @@ from django.conf import settings
 class Advogado(models.Model):
     name = models.CharField(max_length=255, blank=False)
     email = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, null=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="user")
     oab = models.CharField(max_length=12, unique=True, null=False, blank=True)
     image = models.ImageField(default=None, null=True)
-
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    
     def __str__(self) -> str:
-        return f"{self.nome}"
+        return f"{self.name}"
+
+    @property
+    def revenue(self):
+        return sum([gain.profit for gain in self.lawyer.all()])
 
     class Meta:
         verbose_name_plural = 'Advogado'
+        
+        
--- code/advogado/views.py
+++ code/advogado/views.py
@@ -1,26 +1,53 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from advogado.forms import LawyerForm
 from advogado.models import Advogado
+from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def lawyer_create(request: HttpRequest):
-    form = LawyerForm()
-    context = {
-        "form": form
-    }
+    if request.method == "GET":
+        form = LawyerForm()
+        context = {
+            "form": form
+        }
+    else:
+        form = LawyerForm(request.POST, request.FILES)
+        context = {
+            "form": form
+        }
+        if form.is_valid():
+            lawyer = form.save()
+            messages.success(request, f"Advogado {lawyer.name} Registrado com sucesso.")
+            return redirect(reverse("lawyer:lawyer_create"))
+        messages.error(request, "Erro na criação do advogado")
+        return render(request, "lawyer_create.html", context)
+
+        
     return render(request, "lawyer_create.html", context)
     
 
 def lawyer_list(request: HttpRequest):
     lawyers = Advogado.objects.all()
+    paginator = Paginator(lawyers, 10)
+    page_num = request.GET.get('page')
+    page = paginator.get_page(page_num)
     context = {
-        "lawyers": lawyers
+        "lawyers": lawyers,
+        "page": page
     }
     return render(request, "lawyer_list.html", context)
 
-def lawyer_detail(request: HttpRequest):
-    ...
+def lawyer_detail(request: HttpRequest, lawyer_id):
+    lawyer = get_object_or_404(Advogado, pk=lawyer_id)
+    
+    context = {
+        "lawyer": lawyer
+    }
+    
+    return render(request, "lawyer_detail.html", context)
     
 def lawyer_edit(request: HttpRequest):
     ...
