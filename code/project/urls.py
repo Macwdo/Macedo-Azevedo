e8e4a662437505abb06f03c6176709a216570383
macwdo <danilo.macedofernandes@hotmail.com>
2023-12-12 02:48:17 -0300

    :hammer: adding api routes

--- code/project/urls.py
+++ code/project/urls.py
@@ -4,9 +4,25 @@ from django.contrib import admin
 from django.urls import include, path
 
 from advogado.urls import lawyer_router as lawyer_router
-
+from processo.urls import processo_router, processo_router_nested
 
 from project.views import login, logout
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+api_v1_urls = []
+
+api_v1_urls += lawyer_router.urls
+
+api_v1_urls += processo_router.urls
+api_v1_urls += processo_router_nested.urls
+
+api_urls = [
+    path('api/v1/', include(api_v1_urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh_pair')
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +31,8 @@ urlpatterns = [
     path('', include("processo.urls")),
     path('registros/', include("registry.urls")),
     path('advogados/', include("advogado.urls")),
-]
+] + api_urls
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
